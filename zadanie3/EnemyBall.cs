81e5a4deb917841d98bfe42a94149814e1f4d320
Mariusz Piątkowski <mpxx24@gmail.com>
2016-03-24 08:36:04 +0100

    missing class

---
+++ zadanie3/EnemyBall.cs
@@ -0,0 +1,8 @@
+﻿namespace zadanie3 {
+    public class EnemyBall {
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public float SpeedX { get; set; }
+        public float SpeedY { get; set; }
+    }
+}
